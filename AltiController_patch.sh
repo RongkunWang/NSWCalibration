6fce032045d9b22ed8afcf81ca6075a9b44ed3f2
Alexander Naip Tuna <tuna@cern.ch>
2020-08-03 15:28:47 +0200

    added patch files

---
+++ AltiController_patch.sh
@@ -0,0 +1,4 @@
+cd ../AltiController 
+patch -i ../NSWCalibration/AltiController_start_pattern_transition.diff schema/alti.schema.xml 
+patch -i ../NSWCalibration/AltiController_user.diff                     src/AltiController.cpp 
+cd ..
