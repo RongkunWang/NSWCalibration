b54f18fbef87a244694046244fb08013a4ace021
Alexander Naip Tuna <alexander.tuna@cern.ch>
2023-09-28 18:11:13 +0200

    Updates and modernization of pfeb/pt connectivity (sTGCTriggerCalib)

--- src/sTGCTriggerCalib.cpp
+++ src/sTGCTriggerCalib.cpp
@@ -1,7 +1,5 @@
 #include "NSWCalibration/sTGCTriggerCalib.h"
 
-#include "NSWConfiguration/ConfigReader.h"
-#include "NSWConfiguration/ConfigSender.h"
 #include "NSWConfiguration/Constants.h"
 
 #include <cstdlib>
@@ -10,219 +8,190 @@
 
 #include "ers/ers.h"
 
-void nsw::sTGCTriggerCalib::setup(const std::string& db) {
-  ERS_INFO("setup " << db);
+using namespace std::chrono_literals;
 
-  // parse calib type
-  if (m_calibType=="sTGCPadConnectivity") {
-    ERS_INFO(m_calibType);
-  } else if (m_calibType=="sTGCPadLatency") {
-    ERS_INFO(m_calibType);
-  } else {
-    std::stringstream msg;
-    msg << "Unknown calibration request for sTGCTriggerCalib: " << m_calibType << ". Crashing.";
-    ERS_INFO(msg.str());
-    throw std::runtime_error(msg.str());
-  }
-
-  // make NSWConfig objects from input db
-  ERS_INFO("Making pFEB and Pad Trigger objects");
-  m_pfebs = nsw::ConfigReader::makeObjects<nsw::FEBConfig> (db, "PFEB");
-  ERS_INFO("Found " << m_pfebs.size() << " pFEBs");
-  ERS_INFO("Found " << m_pts.get().size()   << " pad triggers");
-  if (m_pts.get().size() != 1) {
-    std::stringstream msg;
-    msg << "I dont know how to process !=1 PadTriggers. You gave: " << m_pts.get().size();
-    ERS_INFO(msg.str());
-    throw std::runtime_error(msg.str());
-  }
-
-  // get list of ordered PFEBs
-  gatherPFEBs();
-
-  // get latency scan parameters from user
-  for (const auto& pt: m_pts.get()) {
-    setLatencyScanOffset(pt.LatencyScanStart());
-    setLatencyScanNBC(pt.LatencyScanNBC());
-  }
-
-  // set number of loops in the iteration
-  if (m_calibType=="sTGCPadConnectivity") {
-    setTotal(static_cast<int>(m_pfebs.size()));
-  } else if (m_calibType=="sTGCPadLatency") {
-    setTotal(latencyScanNBC());
-    ERS_INFO("Latency scan start: " << latencyScanOffset());
-    ERS_INFO("Latency scan steps: " << latencyScanNBC());
-  }
-
-  nsw::snooze();
+nsw::sTGCTriggerCalib::sTGCTriggerCalib(std::string calibType, const nsw::hw::DeviceManager& deviceManager):
+  CalibAlg(std::move(calibType), deviceManager),
+  m_latencyScan{m_calibType == "sTGCPadLatency"}
+{
+  checkCalibType();
+  checkObjects();
+  setTotal(m_latencyScan ? getLatencyScanNBC() : nsw::padtrigger::NUM_PFEBS);
 }
 
 void nsw::sTGCTriggerCalib::configure() {
-  ERS_INFO("sTGCTriggerCalib::configure " << counter());
-  if (m_calibType=="sTGCPadConnectivity") {
-
-    // test pulse one pfeb
-    if (orderPFEBs()) {
-      auto next_addr = nextPFEB(false);
-      for (auto & pfeb: m_pfebs) {
-        if (next_addr == pfeb.getAddress()) {
-          configureVMMs(pfeb, true);
-          break;
-        }
-      }
-    } else {
-      auto & pfeb = m_pfebs.at(static_cast<size_t>(counter()));
-      configureVMMs(pfeb, true);
-    }
-
-    configurePadTrigger();
-    usleep(500e3);
-
-  } else if (m_calibType=="sTGCPadLatency") {
-
+  ERS_INFO("Current feb: " << getCurrentFebName());
+  if (m_latencyScan) {
     // test pulse all pfebs
     if (counter() == 0) {
-      ERS_INFO("sTGCTriggerCalib::configure all pFEBs");
-      for (auto & pfeb: m_pfebs)
-        configureVMMs(pfeb, true);
-    }
-
-    // set readout latency
-    for (const auto& pt: m_pts.get()) {
-      if (!simulation()) {
-        pt.writeReadoutBCOffset(latencyScanCurrent());
+      ERS_LOG("sTGCTriggerCalib::configure all pFEBs");
+      for (const auto& feb: getDeviceManager().getFebs()) {
+        configureVMMs(feb, m_unmask);
       }
     }
     configurePadTrigger();
-    usleep(1e6);
-    usleep(5e6);
+    nsw::snooze();
+  } else {
+    // mask everything to start
+    if (counter() == 0) {
+      ERS_LOG("Masking all PFEB VMM channels");
+      for (const auto& feb: getDeviceManager().getFebs()) {
+        configureVMMs(feb, m_mask);
+      }
+    }
+    // test pulse one pfeb
+    try {
+      configureVMMs(getCurrentFeb(), m_unmask);
+    } catch (const std::exception& ex) {
+      ERS_LOG(fmt::format("{} is absent, continuing",
+                          getCurrentFebName()));
+    }
+    configurePadTrigger();
+    nsw::snooze();
   }
 }
 
 void nsw::sTGCTriggerCalib::unconfigure() {
-  ERS_INFO("sTGCTriggerCalib::unconfigure " << counter());
-
-  if (m_calibType=="sTGCPadConnectivity") {
-    if (orderPFEBs()) {
-      auto next_addr = nextPFEB(true);
-      for (auto & pfeb: m_pfebs) {
-        if (next_addr == pfeb.getAddress()) {
-          configureVMMs(pfeb, false);
-          break;
-        }
-      }
-    } else {
-      auto & pfeb = m_pfebs.at(static_cast<size_t>(counter()));
-      configureVMMs(pfeb, false);
-    }
-
-    usleep(500e3);
-
-  } else if (m_calibType=="sTGCPadLatency") {
-
+  ERS_LOG("sTGCTriggerCalib::unconfigure " << counter());
+  if (m_latencyScan) {
     // mask pulse all pfebs
-    if (counter() == total()-1) {
-      ERS_INFO("sTGCTriggerCalib::unconfigure all pFEBs");
-      for (auto & pfeb: m_pfebs)
-          configureVMMs(pfeb, false);
+    if (counter() == total() - 1) {
+      ERS_LOG("sTGCTriggerCalib::unconfigure all PFEBs");
+      for (const auto& feb: getDeviceManager().getFebs()) {
+        configureVMMs(feb, m_mask);
+      }
+    }
+  } else {
+    try {
+      configureVMMs(getCurrentFeb(), m_mask);
+      nsw::snooze();
+    } catch (const std::exception& ex) {
+      ERS_LOG(fmt::format("Current feb {} is absent, continuing", getCurrentFebName()));
     }
   }
 }
 
-int nsw::sTGCTriggerCalib::configureVMMs(nsw::FEBConfig feb, bool unmask) {
-  ERS_INFO("Configuring " << feb.getOpcServerIp() << " " << feb.getAddress() << " - " << (unmask ? "pulsing" : "masking"));
-  for (auto& vmm : feb.getVmms()) {
-    vmm.setChannelRegisterAllChannels("channel_st", unmask ? 1 : 0);
-    vmm.setChannelRegisterAllChannels("channel_sm", unmask ? 0 : 1);
-  }
-  auto cs = std::make_unique<nsw::ConfigSender>();
-  if (!simulation())
-    cs->sendVmmConfig(feb);
-  return 0;
+std::string_view nsw::sTGCTriggerCalib::getCurrentFebName() const {
+  return nsw::padtrigger::ORDERED_PFEBS_GEOID.at(counter());
 }
 
-int nsw::sTGCTriggerCalib::configurePadTrigger() const {
-  for (const auto& pt: m_pts.get()) {
-    ERS_INFO("Configuring " << pt.getName());
-
-    // enable the L1A readout
-    if (!simulation()) {
-      pt.writeReadoutEnable();
+const nsw::hw::FEB& nsw::sTGCTriggerCalib::getCurrentFeb() const {
+  for (const auto& feb: getDeviceManager().getFebs()) {
+    if (feb.getGeoInfo().resourceType() != "PFEB") {
+      continue;
     }
-
-    // pause to collect L1As
-    nsw::snooze(std::chrono::milliseconds(50));
-
-    // disable the L1A readout
-    if (!simulation()) {
-      pt.writeReadoutDisable();
+    if (nsw::contains(feb.getScaAddress(), getCurrentFebName())) {
+      return feb;
     }
   }
-  return 0;
+  throw std::runtime_error("Couldnt get current FEB");
 }
 
-void nsw::sTGCTriggerCalib::gatherPFEBs() {
-  //
-  // get the partition environment
-  //
-  const auto part = std::getenv("TDAQ_PARTITION");
-  if (part == nullptr) {
-    throw std::runtime_error("Error: TDAQ_PARTITION not defined");
+void nsw::sTGCTriggerCalib::configureVMMs(const nsw::hw::FEB& feb, bool unmask) const {
+  if (feb.getGeoInfo().resourceType() != "PFEB") {
+    return;
   }
-  const std::string partition(part);
-  const std::string sector_name = nsw::guessSector(applicationName());
-  ERS_INFO("Gather PFEBs: found partition "  << partition);
-  ERS_INFO("Gather PFEBs: application "      << applicationName());
-  ERS_INFO("Gather PFEBs: sector name "      << sector_name);
-  ERS_INFO("Gather PFEBs: sector is large: " << nsw::isLargeSector(sector_name));
-  if (partition.find("VS") != std::string::npos) {
-    // VS
-    ERS_INFO("Gather pFEBs: VS pFEBs");
-    m_pfebs_ordered.push_back("/L0/R0");
-    m_pfebs_ordered.push_back("/L1/R0");
-    m_pfebs_ordered.push_back("/L2/R0");
-    m_pfebs_ordered.push_back("/L3/R0");
-    m_pfebs_ordered.push_back("/L0/R1");
-    m_pfebs_ordered.push_back("/L1/R1");
-    m_pfebs_ordered.push_back("/L2/R1");
-    m_pfebs_ordered.push_back("/L3/R1");
-    m_pfebs_ordered.push_back("/L0/R2");
-    m_pfebs_ordered.push_back("/L1/R2");
-    m_pfebs_ordered.push_back("/L2/R2");
-    m_pfebs_ordered.push_back("/L3/R2");
+  if (not feb.getRoc().reachable()) {
+    ERS_LOG("Skipping unreachable " << feb.getScaAddress());
+    return;
   }
-  else {
-    // 191/P1
-    for (const auto& pfeb: nsw::padtrigger::ORDERED_PFEBS_GEOID) {
-      m_pfebs_ordered.push_back(std::string(pfeb));
+  ERS_LOG(fmt::format("Configuring {} ({})", feb.getScaAddress(), (unmask ? "pulsing" : "masking")));
+  for (const auto& vmmDev: feb.getVmms()) {
+    if (vmmDev.getVmmId() == nsw::PFEB_WIRE_VMM) {
+      continue;
+    }
+    auto vmmConf = nsw::VMMConfig{vmmDev.getConfig()};
+    vmmConf.setGlobalRegister("sdp_dac", m_pulse);
+    vmmConf.setGlobalRegister("sdt_dac", m_threshold);
+    vmmConf.setChannelRegisterAllChannels("channel_st", unmask ? true : false);
+    vmmConf.setChannelRegisterAllChannels("channel_sm", unmask ? false : true);
+    try {
+      vmmDev.writeConfiguration(vmmConf);
+    } catch (const std::exception& ex) {
+      ers::warning(nsw::NSWsTGCTriggerCalibIssue(ERS_HERE, ex.what()));
     }
   }
 }
 
-std::string nsw::sTGCTriggerCalib::nextPFEB(bool pop) {
-  if (m_pfebs_ordered.size() == 0) {
-    const auto msg = "Error in nextPFEB. vector is empty, cant get next element.";
-    nsw::NSWsTGCTriggerCalibIssue issue(ERS_HERE, msg);
-    ers::error(issue);
+void nsw::sTGCTriggerCalib::configurePadTrigger() const {
+  for (const auto& pt: getDeviceManager().getPadTriggers()) {
+    ERS_LOG("Configuring " << pt.getName());
+    if (m_latencyScan) {
+      pt.writeReadoutBCOffset(getLatencyScanCurrent());
+    }
+    // L1A based
+    if(m_calibType == "sTGCPadConnectivity" or
+       m_calibType == "sTGCPadLatency") {
+      pt.writeReadoutEnableTemporarily(50ms);
+    // SCA based
+    } else {
+      nsw::snooze(3s);
+      const auto rates = simulation() ?
+        std::vector<std::uint32_t>(nsw::padtrigger::NUM_PFEBS) : pt.readPFEBRates();
+      writeToFile(rates);
+    }
+  }
+}
+
+void nsw::sTGCTriggerCalib::writeToFile(const std::vector<std::uint32_t>& rates) const {
+  const auto fname = fmt::format("{}.{}.{}.txt", m_calibType, runNumber(), applicationName());
+  if (counter() == 0) {
+    ERS_LOG("Opening " << fname);
+  }
+  std::ofstream myfile;
+  myfile.open(fname, std::ios_base::app);
+  std::size_t pfeb{0};
+  for (const auto& rate: rates) {
+    myfile << fmt::format("Iteration {:02} PFEB {:02} {}", counter(), pfeb, rate) << std::endl;
+    ++pfeb;
+  }
+  myfile.close();
+}
+
+std::uint32_t nsw::sTGCTriggerCalib::getLatencyScanOffset() const {
+  if (not m_latencyScan) {
+    return 0;
+  }
+  for (const auto& pt: getDeviceManager().getPadTriggers()) {
+    return pt.LatencyScanStart();
+  }
+  const std::string msg{"No pad triggers in DB"};
+  ers::error(nsw::NSWsTGCTriggerCalibIssue(ERS_HERE, msg));
+  throw std::runtime_error(msg);
+}
+
+std::uint32_t nsw::sTGCTriggerCalib::getLatencyScanNBC() const {
+  if (not m_latencyScan) {
+    return 0;
+  }
+  for (const auto& pt: getDeviceManager().getPadTriggers()) {
+    return pt.LatencyScanNBC();
+  }
+  const std::string msg{"No pad triggers in DB"};
+  ers::error(nsw::NSWsTGCTriggerCalibIssue(ERS_HERE, msg));
+  throw std::runtime_error(msg);
+}
+
+void nsw::sTGCTriggerCalib::checkCalibType() const {
+  if (m_calibType == "sTGCPadConnectivity" or
+      m_calibType == "sTGCPadConnectivitySca" or
+      m_calibType == "sTGCPadLatency") {
+    ERS_INFO(m_calibType);
+  } else {
+    const auto msg = fmt::format("Unknown calibration: {}", m_calibType);
+    ers::error(nsw::NSWsTGCTriggerCalibIssue(ERS_HERE, msg));
     throw std::runtime_error(msg);
   }
-  // get the next valid pfeb
-  // "valid" means: exists in the config
-  const auto next_feb = m_pfebs_ordered.front();
-  for (auto & pfeb: m_pfebs) {
-    if (pfeb.getAddress().find(next_feb) != std::string::npos) {
-      if (pop)
-        m_pfebs_ordered.erase(m_pfebs_ordered.begin());
-      return pfeb.getAddress();
-    }
-  }
-  // if youre here: that pfeb is not in the config db
-  // warn the user, then move to the next pfeb
-  std::string msg = next_feb + " does not exist in config. Skipping.";
-  nsw::NSWsTGCTriggerCalibIssue issue(ERS_HERE, msg);
-  ers::warning(issue);
-
-  m_pfebs_ordered.erase(m_pfebs_ordered.begin());
-  return nextPFEB(pop);
 }
+
+void nsw::sTGCTriggerCalib::checkObjects() const {
+  const auto npts  = getDeviceManager().getPadTriggers().size();
+  const auto nfebs = getDeviceManager().getFebs().size();
+  ERS_INFO(fmt::format("Found {} pad triggers", npts));
+  ERS_INFO(fmt::format("Found {} FEBs", nfebs));
+  if (npts > 1) {
+    const auto msg = fmt::format("sTGCTriggerCalib only works with 1 PT");
+    ers::error(nsw::NSWsTGCTriggerCalibIssue(ERS_HERE, msg));
+  }
+}
+
